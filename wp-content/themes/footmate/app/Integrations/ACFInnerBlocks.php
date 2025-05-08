2243d9c690e5c93588761f000049075017e91ed3
Przemysław Hernik <przemek.hernik@coditive.com>
2025-05-08 21:20:52 +0200

    add inner blocks template

--- wp-content/themes/footmate/app/Integrations/ACFInnerBlocks.php
+++ wp-content/themes/footmate/app/Integrations/ACFInnerBlocks.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace FM\Integrations;
+namespace CB\Integrations;
 
 trait ACFInnerBlocks
 {
     private array $innerBlocks = [
         'active' => false,
         'list' => '',
+        'template' => '',
     ];
 
     public function useInnerBlocks(): void
@@ -24,12 +25,21 @@ trait ACFInnerBlocks
         $this->innerBlocks['list'] = esc_attr(wp_json_encode($blocks));
     }
 
+    public function useInnerBlocksTemplate(array $template): void
+    {
+        $this->innerBlocks['template'] = esc_attr(wp_json_encode($template));
+    }
+
     public function getInnerBlocks(): string
     {
         $arguments = [];
 
         if (! empty($this->innerBlocks['list'])) {
             $arguments[] = sprintf('allowedBlocks="%s"', $this->innerBlocks['list']);
+        }
+
+        if (! empty($this->innerBlocks['template'])) {
+            $arguments[] = sprintf('template="%s"', $this->innerBlocks['template']);
         }
 
         return sprintf('<InnerBlocks %s/>', join(' ', $arguments));
