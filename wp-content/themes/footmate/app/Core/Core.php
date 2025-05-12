e4807a100ad3bec1842e5a87b70d4d21ec614a8d
Przemysław Hernik <przemek.hernik@coditive.com>
2025-05-12 10:24:46 +0200

    add debugger module

--- wp-content/themes/footmate/app/Core/Core.php
+++ wp-content/themes/footmate/app/Core/Core.php
@@ -2,6 +2,14 @@
 
 namespace FM\Core;
 
+use FM\Core\Debugger;
+
 class Core
 {
+    public function __construct()
+    {
+        if (! empty(WP_DEBUG) && ! empty(WP_ENVIRONMENT_TYPE) && WP_ENVIRONMENT_TYPE === 'development') {
+            \FM\App::init(new Debugger());
+        }
+    }
 }
