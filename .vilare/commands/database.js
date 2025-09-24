f54ee9ee79c60a35354303996e82c9278362dd2f
Przemysław Hernik <przemek.hernik@coditive.com>
2025-09-24 21:02:15 +0200

    fix async/await issues

--- .vilare/commands/database.js
+++ .vilare/commands/database.js
@@ -60,7 +60,7 @@ export class Controller {
     }
   }
 
-  export(environment) {
+  async export(environment) {
     if (fs.existsSync(`${this.wordpress.path}/db.sql.gz`)) {
       shell.exec(`rm ${this.wordpress.path}/db.sql.gz`);
     }
@@ -76,17 +76,17 @@ export class Controller {
         break;
 
       case 'staging':
-        shell.exec(`wp search-replace ${config('DOMAIN_LOCAL')} ${config('DOMAIN_STAGING')} --all-tables`);
+        shell.exec(`wp search-replace ${await config('DOMAIN_LOCAL')} ${await config('DOMAIN_STAGING')} --all-tables`);
         shell.exec(`wp db export ${this.wordpress.path}/db.sql`);
         shell.exec(`gzip ${this.wordpress.path}/db.sql`);
-        shell.exec(`wp search-replace ${config('DOMAIN_STAGING')} ${config('DOMAIN_LOCAL')} --all-tables`);
+        shell.exec(`wp search-replace ${await config('DOMAIN_STAGING')} ${await config('DOMAIN_LOCAL')} --all-tables`);
         break;
 
       case 'production':
-        shell.exec(`wp search-replace ${config('DOMAIN_LOCAL')} ${config('DOMAIN_PROD')} --all-tables`);
+        shell.exec(`wp search-replace ${await config('DOMAIN_LOCAL')} ${await config('DOMAIN_PROD')} --all-tables`);
         shell.exec(`wp db export ${this.wordpress.path}/db.sql`);
         shell.exec(`gzip ${this.wordpress.path}/db.sql`);
-        shell.exec(`wp search-replace ${config('DOMAIN_PROD')} ${config('DOMAIN_LOCAL')} --all-tables`);
+        shell.exec(`wp search-replace ${await config('DOMAIN_PROD')} ${await config('DOMAIN_LOCAL')} --all-tables`);
         break;
 
       default:
@@ -94,7 +94,7 @@ export class Controller {
     }
   }
 
-  import(environment) {
+  async import(environment) {
     if (fs.existsSync(`${this.wordpress.path}/db.sql.gz`)) {
       shell.exec(`gzip -d ${this.wordpress.path}/db.sql.gz`);
     }
@@ -109,11 +109,11 @@ export class Controller {
 
     switch (environment) {
       case 'staging':
-        shell.exec(`wp search-replace ${config('DOMAIN_STAGING')} ${config('DOMAIN_LOCAL')} --all-tables`);
+        shell.exec(`wp search-replace ${await config('DOMAIN_STAGING')} ${await config('DOMAIN_LOCAL')} --all-tables`);
         break;
 
       case 'production':
-        shell.exec(`wp search-replace ${config('DOMAIN_PROD')} ${config('DOMAIN_LOCAL')} --all-tables`);
+        shell.exec(`wp search-replace ${await config('DOMAIN_PROD')} ${await config('DOMAIN_LOCAL')} --all-tables`);
         shell.exec('wp option set blog_public 0');
         shell.exec('wp user update $(wp user list --field=ID) --user_pass=test1234 --skip-email');
         break;
@@ -123,7 +123,7 @@ export class Controller {
     }
   }
 
-  optimize() {
+  async optimize() {
     shell.exec('wp post delete $(wp post list --post_type=\'revision\' --format=ids) --force');
     shell.exec('wp db query "DELETE pm FROM wp_postmeta pm LEFT JOIN wp_posts wp ON wp.ID = pm.post_id WHERE wp.ID IS NULL;"');
     shell.exec('wp db query "DELETE FROM wp_commentmeta WHERE comment_id NOT IN (SELECT comment_id FROM wp_comments);"');
@@ -149,9 +149,9 @@ export const database = () => {
     .command('export')
     .description('export database')
     .option('-e, --env <env>', 'local | staging | production', 'local')
-    .action((options) => {
+    .action(async(options) => {
       try {
-        controller.export(options.env);
+        await controller.export(options.env);
       } catch (error) {
         program.error(error);
       }
@@ -161,9 +161,9 @@ export const database = () => {
     .command('import')
     .description('import database')
     .option('-e, --env <env>', 'local | staging | production', 'local')
-    .action((options) => {
+    .action(async(options) => {
       try {
-        controller.import(options.env);
+        await controller.import(options.env);
       } catch (error) {
         program.error(error);
       }
@@ -172,9 +172,9 @@ export const database = () => {
   program
     .command('optimize')
     .description('optimize database')
-    .action(() => {
+    .action(async() => {
       try {
-        controller.optimize();
+        await controller.optimize();
       } catch (error) {
         program.error(error);
       }
